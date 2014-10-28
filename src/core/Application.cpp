c510f7037a16ec55228fc532183ab3ddec1e1125
Sebastian Szymak <sebastian.szymak@gmail.com>
2014-10-29 00:54:57 +0100

    Initial project setup.

---
+++ src/core/Application.cpp
@@ -0,0 +1,27 @@
+/*
+ * Application.cpp
+ *
+ *  Created on: 29 paź 2014
+ *      Author: piesbert
+ */
+
+#include "Application.h"
+
+namespace ymine {
+namespace core {
+
+Application::Application() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Application::~Application() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace core */
+} /* namespace ymine */
+
+int main(int argc, char *argv[]) {
+	return 0;
+}
---
+++ src/core/Application.h
@@ -0,0 +1,23 @@
+/*
+ * Application.h
+ *
+ *  Created on: 29 paź 2014
+ *      Author: piesbert
+ */
+
+#ifndef SRC_CORE_APPLICATION_H_
+#define SRC_CORE_APPLICATION_H_
+
+namespace ymine {
+namespace core {
+
+class Application {
+public:
+	Application();
+	virtual ~Application();
+};
+
+} /* namespace core */
+} /* namespace ymine */
+
+#endif /* SRC_CORE_APPLICATION_H_ */
