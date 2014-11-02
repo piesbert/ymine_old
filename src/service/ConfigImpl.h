e3b468668559e948a0192fdbc41cd427dcbabd4c
Sebastian Szymak <sebastian.szymak@gmail.com>
2014-11-02 19:09:33 +0100

    Comments update.
    Add N/A.
    
    Update file comments.
    
    Fix N/A.

--- src/service/ConfigImpl.h
+++ src/service/ConfigImpl.h
@@ -1,8 +1,23 @@
-/*
- * ConfigImpl.h
+/* File:       ConfigImpl.h
+ * Project:    ymine
+ * Created on: Oct 29, 2014 01:00:00 AM
+ * Author:     piesbert
  *
- *  Created on: 29 paź 2014
- *      Author: piesbert
+ * Copyright (C) 2014 Sebastian Szymak
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 #ifndef SRC_SERVICE_CONFIGIMPL_H_
